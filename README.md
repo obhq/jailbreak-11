b1138c2744f55df5b8ea5e90e1c0424fbb3ec5d0
Putta Khunchalee <ultimaweapon@outlook.com>
2024-04-28 00:13:37 +0700

    Binds PPPoE discovery socket to a specific interface

--- README.md
+++ README.md
@@ -15,11 +15,28 @@ You need to connect the PS4 and the computer with an ethernet cable **without** 
 
 ## Running
 
-You need to run the following command as a root on the computer that was setup by the above instruction:
+Run the following command on the computer that connect with the PS4 to find the index of the ethernet port connected with the PS4:
 
 ```sh
-sudo ./jailbreak-11
+ip link
 ```
+
+It will output something like:
+
+```
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+2: enp3s0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP mode DEFAULT group default qlen 1000
+    link/ether ??:??:??:??:??:?? brd ff:ff:ff:ff:ff:ff
+```
+
+My computer has only one ethernet so `2` is the index of the ethernet port for me. Once figure out run the following command:
+
+```sh
+sudo ./jailbreak-11 INDEX
+```
+
+Replace `INDEX` with the ethernet index then open the PS4 and go to `Settings > Network > Set Up Internet Connection > Use a LAN Cable > Custom > PPPoE`. Enter a random `PPPoE User ID` and `PPPoE Password`.
 
 ## License
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-use libc::{socket, AF_PACKET, ETH_P_PPP_DISC, SOCK_RAW};
+use clap::{command, value_parser, Arg};
+use libc::{bind, sockaddr_ll, socket, AF_PACKET, ETH_P_PPP_DISC, SOCK_RAW};
+use std::ffi::c_int;
 use std::io::Error;
-use std::os::fd::{FromRawFd, OwnedFd};
+use std::mem::{size_of_val, zeroed};
+use std::os::fd::{AsRawFd, FromRawFd, OwnedFd};
 use std::process::ExitCode;
 
 fn main() -> ExitCode {
+    // Parse arguments.
+    let args = command!()
+        .arg(
+            Arg::new("interface")
+                .help("Index of the interface that connected with the PS4")
+                .value_name("IF")
+                .value_parser(value_parser!(c_int))
+                .required(true),
+        )
+        .get_matches();
+
     // Create a socket for PPPoE discovery.
-    let disc = unsafe { socket(AF_PACKET, SOCK_RAW, ETH_P_PPP_DISC.to_be()) };
+    let disc = unsafe { socket(AF_PACKET, SOCK_RAW, 0) };
 
     if disc < 0 {
         eprintln!(
@@ -16,8 +30,28 @@ fn main() -> ExitCode {
         return ExitCode::FAILURE;
     }
 
-    // Bind socket to a specific interface.
+    // Bind socket to target interface.
     let disc = unsafe { OwnedFd::from_raw_fd(disc) };
+    let mut addr: sockaddr_ll = unsafe { zeroed() };
+
+    addr.sll_family = AF_PACKET as _;
+    addr.sll_protocol = ETH_P_PPP_DISC.to_be() as _;
+    addr.sll_ifindex = *args.get_one("interface").unwrap();
+
+    if unsafe {
+        bind(
+            disc.as_raw_fd(),
+            &addr as *const sockaddr_ll as _,
+            size_of_val(&addr).try_into().unwrap(),
+        ) < 0
+    } {
+        eprintln!(
+            "Failed to bind PPPoE discovery socket: {}.",
+            Error::last_os_error()
+        );
+
+        return ExitCode::FAILURE;
+    }
 
     ExitCode::SUCCESS
 }
